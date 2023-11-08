75324f6db4ed91f1b8e5171d516ad34330b71e12
h8008 <hugh80082@gmail.com>
2023-11-08 09:55:18 +0000

    Updated cors options

--- server/app/Middleware/CORS.js
+++ server/app/Middleware/CORS.js
@@ -1,10 +1,17 @@
 const cors = require('kcors');
 
-module.exports = function (app) {
+const corsOptions = {
+    credentials: true,
+    // origin: `https://portfolio-two-taupe-78.vercel.app`,
+    origin: "*",
+    exposeHeaders: ["Access-Token", "Cookie"],
+    allowMethods: ["OPTIONS", "POST", "GET", "PATCH", "PUT"],
+    allowHeaders:
+      "Access-Control-Allow-Credentials, Access-Control-Allow-Origin, Access-Control-Allow-Methods, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  };
 
+
+module.exports = function (app) {
     // Send standard CORS headers with all origins allowed                                                                                                
-    app.use(cors({
-            credentials: true,
-            exposeHeaders: ['Access-Token', 'Cookie']
-            }));
+    app.use(cors(corsOptions));
 };
