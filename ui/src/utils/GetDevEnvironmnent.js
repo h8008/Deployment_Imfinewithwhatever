ae49416fc1b07c4bfc10c96633c0bc2b6f857152
Huiran Lin <hugh80082@gmail.com>
2023-11-09 01:38:09 -0800

    Updated correct ui environment variable

--- ui/src/utils/GetDevEnvironmnent.js
+++ ui/src/utils/GetDevEnvironmnent.js
@@ -1,6 +1,6 @@
 const getDevEnvironment = () => {
   const dev =
-    process.env.REACT_APP_DEVELOPMENT === null
+    process.env.REACT_APP_DEVELOPMENT == null
       ? ''
       : process.env.REACT_APP_DEVELOPMENT;
   return dev.trim() === 'true';
