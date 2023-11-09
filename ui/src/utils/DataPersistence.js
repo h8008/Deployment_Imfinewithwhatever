9796fbf47445c1774e0b6ad3cbfc904ff95e4e06
Huiran Lin <hugh80082@gmail.com>
2023-11-08 23:47:06 -0800

    Updated ui npm scripts

--- ui/src/utils/DataPersistence.js
+++ ui/src/utils/DataPersistence.js
@@ -1,28 +1,26 @@
-import { arrayToString, objectToString } from "./DataExtraction";
-import Cookies from "js-cookie";
+import { arrayToString, objectToString } from './DataExtraction';
+import Cookies from 'js-cookie';
 
 const storeCookie = async (name, object) => {
   const string = await Promise.all(
     Object.values(object).map(async (value) => {
       if (Array.isArray(value)) {
         const string = await arrayToString(value);
-        console.log("string", string);
+        console.log('string', string);
         return string;
-      } else if (typeof value === "object") {
-        console.log("value", value);
+      } else if (typeof value === 'object') {
+        console.log('value', value);
         const string = await objectToString(value);
-        console.log("string", string);
+        console.log('string', string);
         return string;
       }
-      console.log("string", value);
+      console.log('string', value);
       return value;
     })
   ).then((string) => string);
 
-  console.log("stringified object", string);
+  console.log('stringified object', string);
   Cookies.set(name, string);
 };
-
-const storeCookie2 = async (name, object) => {};
 
 export { storeCookie };
