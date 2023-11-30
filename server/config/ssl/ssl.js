d26074c6b983c4c62f4627f5f1146ecb0790098d
Huiran Lin <hugh80082@gmail.com>
2023-11-30 11:56:59 -0800

    Switches to using http server on vercel

--- server/config/ssl/ssl.js
+++ server/config/ssl/ssl.js
@@ -2,9 +2,9 @@ const https = require('https');
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const getDevEnvironment = require('../../utils/GetDevEnvironment.js')
+const getDevEnvironment = require('../../utils/GetDevEnvironment.js');
 
 module.exports = function (app) {
-    return getDevEnvironment() ? http.createServer(app) : https.createServer(app)
-}
-
+  // return getDevEnvironment() ? http.createServer(app) : https.createServer(app)
+  return http.createServer(app);
+};
