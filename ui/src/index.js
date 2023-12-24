12542e903e771d1c4a8192e1dc7d059e313d11d6
Hui Lin <hugh80082@gmail.com>
2023-12-24 08:32:09 -0800

    Added vercel analytics

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import RootLayout from "./VercelAnalytics";
+
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <App />
+  <>
+    <App />
+    <Analytics />
+  </>
   // </React.StrictMode>
 );
 
