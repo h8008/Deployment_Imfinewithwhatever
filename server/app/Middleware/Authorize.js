773f7da6eac6e5863cd3ab9a3b66c4bd6789a6bb
Hui Lin <hugh80082@gmail.com>
2023-12-26 15:33:31 -0800

    Revert responsiveness change

--- server/app/Middleware/Authorize.js
+++ server/app/Middleware/Authorize.js
@@ -4,13 +4,13 @@ const setAccessToken = require('../../config/setAccessToken');
 
 module.exports = (min_type) => {
   return async (ctx, next) => {
-    console.log('min_type in authorize is', min_type);
+    // console.log('min_type in authorize is', min_type);
 
-    console.log('In Authorize. ctx.state = ', ctx.state);
-    console.log('In Authorize. ctx.state.jwtdata = ', ctx.state.jwtdata);
+    // console.log('In Authorize. ctx.state = ', ctx.state);
+    // console.log('In Authorize. ctx.state.jwtdata = ', ctx.state.jwtdata);
 
-    console.log('ctx.body', ctx.body);
-    console.log('ctx.body.data', ctx.body.data);
+    // console.log('ctx.body', ctx.body);
+    // console.log('ctx.body.data', ctx.body.data);
 
     const user = ctx.body.data;
     setAccessToken(ctx, user);
