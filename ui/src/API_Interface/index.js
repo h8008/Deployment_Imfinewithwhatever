4e7c151b0bad74abb9a294be81574b6de7f939a7
Hui Lin <hugh80082@gmail.com>
2023-12-29 12:34:21 -0800

    Use verify jwt on app level

--- ui/src/API_Interface/index.js
+++ ui/src/API_Interface/index.js
@@ -11,7 +11,8 @@ const AxiosConfigured = () => {
   // axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
   // Set the baseURL for all requests to the API domain instead of the current domain
   // axios.defaults.baseURL = `http://localhost:8443/api/v1`;
-  axios.defaults.baseURL = axiosBaseUrl();
+  // axios.defaults.baseURL = axiosBaseUrl();
+  axios.defaults.baseURL = `https://deployment-imfinewithwhatever-test-server`
   // Allow the browser to send cookies to the API domain (which include auth_token)
   axios.defaults.withCredentials = true;
   //    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf_token;
