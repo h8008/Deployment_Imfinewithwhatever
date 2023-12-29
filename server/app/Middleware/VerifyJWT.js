2fc8a8bc2a38f426715a5924ad268784e72c71da
Hui Lin <hugh80082@gmail.com>
2023-12-29 11:59:40 -0800

    add function to extract access token from ctx req object

--- server/app/Middleware/VerifyJWT.js
+++ server/app/Middleware/VerifyJWT.js
@@ -18,12 +18,21 @@ module.exports = async (ctx, next) => {
     // const user = await User.findOne().where("email").equals(email)
     // const token = user.access_token
 
-    const token = ctx.cookie.access_token
+    // const token = ctx.cookie.access_token
+
+    // console.log("token", token[0])
+
+    // console.log(ctx.req.headers.cookie)
+
+    const token = Object.fromEntries(ctx.req.headers.cookie.split("; ").map((cookie) => {
+        const pair = cookie.split("=")
+        return [pair[0], pair[1]]
+    }))["access_token"]
+
 
     console.log("token", token[0])
 
     
-
     return jwt.verify(token, process.env.JWT_KEY, async function(err, decoded) {
         if (err) return;
         const exp = decoded.exp < Date.now().valueOf() / 1000
